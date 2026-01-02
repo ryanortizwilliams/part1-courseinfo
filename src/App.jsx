ee29bec65bd5af5f1e476a32033b1bdc86b88785
ryanortizwilliams <ryanortizwilliams@outlook.com>
2026-01-02 14:17:19 +0900

    refactor content section

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,29 @@ const Header = (props) => {
 };
 
 // Content
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts[0].partName} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].partName} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].partName} {props.parts[2].exercises}
+      </p>
+    </>
+  );
+};
 
 // Total
 function App() {
   const course = "Half Stack application development";
   const parts = [
     { partName: "Fundamentals of React", exercises: 10 },
-    { partName: "Using props to pass data", exercises: 10 },
+    { partName: "Using props to pass data", exercises: 7 },
+    { partName: "State of a component", exercises: 14 },
   ];
   const part1 = "Fundamentals of React";
   const exercises1 = 10;
@@ -29,15 +45,7 @@ function App() {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content parts={parts} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
